dde54427d43d28c4e2ae45e745f1a0d0b2918727
CM Geldenhuys <4649249+CMGeldenhuys@users.noreply.github.com>
2025-03-10 09:27:13 +0200

    hk(hubert): add pbar for label prediction

--- examples/hubert/utils/kmeans.py
+++ examples/hubert/utils/kmeans.py
@@ -205,6 +205,7 @@ def get_km_label(
     split: str,
     num_rank: int,
     device: torch.device,
+    silent_pbar: bool = False,
 ) -> None:
     r"""Predict the labels by the KMeans clustering model.
     Args:
@@ -225,7 +226,7 @@ def get_km_label(
     label_path = label_dir / f"label_{split}.pt"
     apply_kmeans = ApplyKmeans(km_path, device)
     with open(label_path, "w") as f:
-        for rank in range(1, num_rank + 1):
+        for rank in tqdm(range(1, num_rank + 1), desc="Predict KMeans label", unit="rank", disable=silent_pbar):
             offset = 0
             feat_path, len_path = _get_feat_lens_paths(feat_dir, split, rank, num_rank)
             feats = torch.load(feat_path, weights_only=True)
